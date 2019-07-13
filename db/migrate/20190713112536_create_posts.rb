bcbd31247f08db70574d47b569ea59d1f2b67913
katsura13 <kadokyu13@gmail.com>
2019-07-13 20:48:38 +0900

    Postモデル作成 Bootstrap導入

---
+++ db/migrate/20190713112536_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.string :title, null: false, length: 50
+      t.text :body,    null: false, length: 2000
+      
+      t.timestamps
+    end
+  end
+end
