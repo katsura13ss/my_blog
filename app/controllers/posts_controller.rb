288bebfed28340b09d0ce7354d3013a8f1c329f8
katsura13 <kadokyu13@gmail.com>
2019-07-14 16:11:33 +0900

    user関連 完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params) 
+    @post.attributes = {user_id: current_user.id}
     if @post.save 
       redirect_to @post, notice: "ブログを登録しました。" 
     else 
