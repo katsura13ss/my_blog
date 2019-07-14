af4ec7a1b54861d818e4f746f0535acb0ee3bb8d
katsura13 <kadokyu13@gmail.com>
2019-07-14 18:14:04 +0900

    ページベーションと検索機能 完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show,  :edit, :update, :destroy]
   def index
-    @posts = Post.page(params[:page]).order(created_at: :desc).per(4)
     @new_posts = Post.order(created_at: :desc).limit(5)
   end
 
